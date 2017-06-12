729d8211d0baf6bdd2741234a62b2948b78f2c9e
陈晨昕 <bug_wacko@163.com>
2017-06-12 18:28:33 +0800

    Initial Commit

---
+++ CXPhotoBrower/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CXPhotoBrower
+//
+//  Created by 陈晨昕 on 2017/6/12.
+//  Copyright © 2017年 bugWacko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CXPhotoBrowerTests/CXPhotoBrowerTests.m
@@ -0,0 +1,39 @@
+//
+//  CXPhotoBrowerTests.m
+//  CXPhotoBrowerTests
+//
+//  Created by 陈晨昕 on 2017/6/12.
+//  Copyright © 2017年 bugWacko. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CXPhotoBrowerTests : XCTestCase
+
+@end
+
+@implementation CXPhotoBrowerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
